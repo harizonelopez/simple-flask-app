2f5b146ff3a61e8bcc78fcb93cce8e334a5ca6eb
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-07-30 11:34:50 +0300

    Update auth.py

--- website/auth.py
+++ website/auth.py
@@ -2,27 +2,23 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/note', methods = ['GET', 'POST'])
+@auth.route('/note', methods=['GET', 'POST'])
 def note():
     data = request.form
     print(data)
-    
-    if request.method == 'POST':
+    if request.method=='POST':
         notes = request.form.get('notes')
-        
         if len(notes) < 10:
             flash('The note must have more than 10 characters.', category='error')
-
         else:
             flash('The note has been successfully recorded')
     
     return render_template("index.html")
 
-@auth.route('/contact', methods= ['GET', 'POST'])
+@auth.route('/contact', methods=['GET', 'POST'])
 def contact():
-    if request.method == 'POST':
+    if request.method=='POST':
         return redirect(url_for('views.home'))
     
     return render_template("contact.html")
-    
     
