462871bd98e6419294471aaee1e2652e3f08d7fc
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-07-30 11:36:08 +0300

    Update views.py

--- website/views.py
+++ website/views.py
@@ -2,18 +2,10 @@ from flask import Flask, request, redirect, url_for, Blueprint, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 views = Blueprint('views', __name__)
-
-
-@views.route('/', methods = ['POST', 'GET'])
+@views.route('/', methods=['POST', 'GET'])
 def home():
-    if request.method == 'POST':
+    if request.method=='POST':
         return redirect(url_for('auth.contact'))
         
     return render_template("home.html")
-
-
-
-       
-   
-    
     
