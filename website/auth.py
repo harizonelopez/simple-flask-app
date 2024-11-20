40bcbeaae00c2a752e696c0026ac27092790e946
Harizone lopez <136503275+harizonelopez@users.noreply.github.com>
2024-11-20 15:00:20 +0300

    Updates

--- website/auth.py
+++ website/auth.py
@@ -2,11 +2,11 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/note', methods=['GET', 'POST'])
+@auth.route('/note', methods=["GET", "POST"])
 def note():
     data = request.form
     print(data)
-    if request.method=='POST':
+    if request.method=="POST":
         notes = request.form.get('notes')
         if len(notes) < 10:
             flash('The note must have more than 10 characters.', category='error')
@@ -15,9 +15,9 @@ def note():
     
     return render_template("index.html")
 
-@auth.route('/contact', methods=['GET', 'POST'])
+@auth.route('/contact', methods=["GET", "POST"])
 def contact():
-    if request.method=='POST':
+    if request.method=="POST":
         return redirect(url_for('views.home'))
     
     return render_template("contact.html")
